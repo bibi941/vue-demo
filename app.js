4ab3b5c12d3f89eead1ce9e89bf6e0b8bcff78ba
finley <sssrrr@vip.qq.com>
2018-04-10 22:05:18 +0800

    添加css样式

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import Vue from 'vue'
+import AV from 'leancloud-storage'
 
-var app = new Vue({
+let APP_ID = 'I1vRQRic7Nb5TeYbck2bdUP0-gzGzoHsz'
+let APP_KEY = 'AVetKsDeAS7CpFWNbbjzCBuk'
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+})
+
+let app = new Vue({
     el: '#app',
     data: {
+        isActive: true,
         newTodo: '',
         todoList: [],
-        title: 'To Do List'
+        title: 'To Do List',
+        actionType: 'signUp',
+        formData: {
+            username: '',
+            password: ''
+        },
+        currentUser: null
     },
     created() {
         window.onbeforeunload = () => {
@@ -21,25 +37,35 @@ var app = new Vue({
         let oldTodo = JSON.parse(oldTodoString)
         this.todoList = oldData || []
         this.newTodo = oldTodo || ''
+        this.currentUser = this.getCurrentUser()
     },
     methods: {
+        change(data) {
+            if (data === 'signUp') {
+                this.actionType = 'signUp'
+            } else if (data === 'signIn') {
+                this.actionType = 'signIn'
+            }
+            this.isActive = !this.isActive //change-Active
+        },
         addTodo() {
             if (!this.newTodo) {
                 return
-            }//input不能为空
+            } //input不能为空
             this.todoList.push({
                 title: this.newTodo,
                 createdAt: new Date(),
                 date: (function () {
-                    var d = new Date()
-                    var year = d.getFullYear()
-                    var month = d.getMonth() + 1
-                    var day = d.getDate() < 10 ? '0' + d.getDate() : '' + d.getDate()
-                    var hour = d.getHours()
-                    var minutes = d.getMinutes()
-                    var seconds = d.getSeconds()
-                    return 'TIME:'+year + '.' + month + '.' + day + ' ' + hour + ':' + minutes + ':' + seconds
-                }()),
+                    let d = new Date()
+                    let year = d.getFullYear()
+                    let month = d.getMonth() + 1
+                    let day =
+                        d.getDate() < 10 ? '0' + d.getDate() : '' + d.getDate()
+                    let hour = d.getHours()
+                    let minutes = d.getMinutes()
+                    let seconds = d.getSeconds()
+                    return ('TIME:' + year + '.' + month + '.' + day + ' ' + hour + ':' + minutes + ':' + seconds)
+                })(),
                 done: false
             })
             this.newTodo = ''
@@ -47,6 +73,44 @@ var app = new Vue({
         removeTodo(todo) {
             let index = this.todoList.indexOf(todo) //在todoList中的index
             this.todoList.splice(index, 1) //删除这个todo,从index开始，删除1个
+        },
+        signUp() {
+            let user = new AV.User()
+            // 设置用户名
+            user.setUsername(this.formData.username)
+            // 设置密码
+            user.setPassword(this.formData.password)
+            user.signUp().then(
+                loginedUser => {
+                    alert('注册成功！即将跳转页面')
+                    this.currentUser = this.getCurrentUser()
+                },
+                error => {
+                    alert('注册失败')
+                }
+            )
+        },
+        login() {
+            AV.User.logIn(this.formData.username, this.formData.password).then(
+                loginedUser => {
+                    this.currentUser = this.getCurrentUser()
+                },
+                function (error) {}
+            )
+        },
+        getCurrentUser() {
+            let current = AV.User.current()
+            if (current) {
+                let {id,createdAt,attributes: {username}} = current
+                return {id,username,createdAt}
+            } else {
+                return null
+            }
+        },
+        logout() {
+            AV.User.logOut()
+            this.currentUser = null
+            window.location.reload()
         }
     }
 })
